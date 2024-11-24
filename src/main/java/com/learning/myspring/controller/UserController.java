ae9ee81d61024346d82550e938678a7bd19d96f6
Anish <mnanish55@gmail.com>
2024-11-24 13:03:15 +0530

    Add an endpoint

--- src/main/java/com/learning/myspring/controller/UserController.java
+++ src/main/java/com/learning/myspring/controller/UserController.java
@@ -33,6 +33,11 @@ public class UserController {
 
     @GetMapping("/sayHi")
     public String sayHii(){
-        return "sayHii";
+        return "Hii";
+    }
+
+    @GetMapping("/sayGoodBye")
+    public String sayBye(){
+        return "GoodBye";
     }
 }
